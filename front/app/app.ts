9fd7e18a281a4f90d3b366c61107aa281baf30e3
Thomas Queste <tom@tomsquest.com>
2017-01-06 11:50:59 +0100

    Change loading text on app load
    May use Dom loaded event instead of putting the JS at the end...

--- front/app/app.ts
+++ front/app/app.ts
@@ -1,13 +1,8 @@
-import {Bar} from "./foo/bar";
-
 class App {
-
     constructor() {
         const app = document.getElementById("app");
         console.log('App loaded', app);
-
-        const bar = new Bar();
-        console.log('Bar ?', bar.do())
+        app.innerText = "Loaded !"
     }
 }
 
