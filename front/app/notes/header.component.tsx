d78cb80235e57050d7b61c392f6b7d80aa9c57ee
Thomas Queste <tom@tomsquest.com>
2017-01-17 13:58:06 +0100

    Initial Notes component using flexbox

--- front/app/notes/header.component.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-export class HeaderComponent extends React.Component {
-    render() {
-        return <div className="header">
-            <h2>Notes</h2>
-        </div>
-    }
-}
--- front/app/notes/notes.component.tsx
+++ front/app/notes/notes.component.tsx
@@ -1,12 +1,23 @@
 import * as React from "react";
-import {HeaderComponent} from "./header.component";
-import {ContentComponent} from "./content.component";
 
 export class NotesComponent extends React.Component {
     render() {
-        return <div className="notes">
-            <HeaderComponent />
-            <ContentComponent />
-        </div>
+        return (
+            <div className="notes-container">
+                <header className="notes-header">
+                    <div className="notes-header-icon">Icon</div>
+                    <div className="notes-header-title">Title</div>
+                    <div className="notes-header-button">Search Button</div>
+                </header>
+                <div className="notes-content">
+                    <div className="notes-item">Item 1</div>
+                    <div className="notes-item">Item 2</div>
+                    <div className="notes-item">Item 3</div>
+                </div>
+                <footer className="notes-footer">
+                    Footer
+                </footer>
+            </div>
+        )
     }
 }
