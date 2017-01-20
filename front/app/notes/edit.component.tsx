d6ee6c21486a888dfc290940787047d9d7a6ea8c
Thomas Queste <tom@tomsquest.com>
2017-01-20 10:54:22 +0100

    Add last saved and save panel

--- front/app/notes/edit.component.tsx
+++ front/app/notes/edit.component.tsx
@@ -6,7 +6,8 @@ export default class Edit extends React.Component {
             <div className="notes-edit-container">
                 <textarea className="notes-edit-textarea" placeholder="Edit this note"/>
                 <footer className="notes-edit-footer">
-                    Footer
+                    <span className="notes-edit-footer-lastsaved">last saved 10 min ago</span>
+                    <div className="notes-edit-footer-save">Save</div>
                 </footer>
             </div>
         )
