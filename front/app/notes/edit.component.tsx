b3b1da1b6a828568121b54375d57a39d40b565d0
Thomas Queste <tom@tomsquest.com>
2017-01-20 10:23:42 +0100

    Display textarea and its footer

--- front/app/notes/edit.component.tsx
+++ front/app/notes/edit.component.tsx
@@ -4,7 +4,10 @@ export default class Edit extends React.Component {
     render() {
         return (
             <div className="notes-edit-container">
-                Edit me
+                <textarea className="notes-edit-textarea" placeholder="Edit this note"/>
+                <footer className="notes-edit-footer">
+                    Footer
+                </footer>
             </div>
         )
     }
