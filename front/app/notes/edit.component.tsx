5cc16e24482bc75cd0aa9eb9fc44371157786307
Thomas Queste <tom@tomsquest.com>
2017-01-20 11:02:43 +0100

    Set the container as a form

--- front/app/notes/edit.component.tsx
+++ front/app/notes/edit.component.tsx
@@ -3,13 +3,13 @@ import * as React from "react";
 export default class Edit extends React.Component {
     render() {
         return (
-            <div className="notes-edit-container">
+            <form className="notes-edit-container">
                 <textarea className="notes-edit-textarea" placeholder="Edit this note"/>
                 <footer className="notes-edit-footer">
                     <span className="notes-edit-footer-lastsaved">last saved 10 min ago</span>
                     <div className="notes-edit-footer-save">Save</div>
                 </footer>
-            </div>
+            </form>
         )
     }
 }
